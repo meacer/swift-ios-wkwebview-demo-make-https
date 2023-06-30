45deadcd189015cdc0f94fd04ebddd6da01b7f9d
Mustafa Emre Acer <meacer@chromium.org>
2023-06-29 18:57:30 -0700

    Mixed content with make-https example

--- wkwebview/ViewController.swift
+++ wkwebview/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 import WebKit
 
-class ViewController: UIViewController, WKNavigationDelegate {
+class ViewController: UIViewController {
     var webView: WKWebView!
 
     override func loadView() {
@@ -19,11 +19,48 @@ class ViewController: UIViewController, WKNavigationDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let url = URL(string: "https://www.google.com")!
+
+        // Set a content filter:
+      let blockRules = """
+          [{
+              "trigger": {
+                  "url-filter": "http://.*",
+                  "resource-type": ["image", "media"],
+                  "if-top-url" : ["https://.*" ]
+              },
+              "action": {
+                  "type": "make-https"
+              }
+          }]
+       """
+      WKContentRuleListStore.default().compileContentRuleList(
+          forIdentifier: "ContentBlockingRules",
+          encodedContentRuleList: blockRules) { (contentRuleList, error) in
+              assert (error == nil)
+              let configuration = self.webView.configuration
+              configuration.userContentController.add(contentRuleList!)
+      }
+
+        // HTTP image on an HTTPS page:
+        // let url = URL(string: "https://www.mixedcontentexamples.com/Test/NonSecureImage")!
+
+        // HTTP Image with a dynamic response:
+         let url = URL(string: "https://regular-puzzling-leo.glitch.me")!
+
+        webView.navigationDelegate = self
         webView.load(URLRequest(url: url))
         webView.allowsBackForwardNavigationGestures = true
     }
 
-
 }
 
+extension ViewController: WKNavigationDelegate {
+
+  func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+    print("Finished navigating to url: \(webView.url)")
+
+    // webView.hasOnlySecureContent should be true with make-https:
+    print("Has only secure content: \(webView.hasOnlySecureContent)");
+  }
+
+}
